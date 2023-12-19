b36bae720229b2381eb7898d60534fe30d6a75bc
sujay gawas <98768459+Sujaygawas07@users.noreply.github.com>
2023-12-19 13:17:45 +0530

    Add files via upload

---
+++ Calculate sum of the elements of the array.py
@@ -0,0 +1,9 @@
+array=list(map(int,input("enter the no nu space:-").split())) # taking input as an array
+#print(array)
+
+
+#new_array=[sun for sum in array if num%2==0]           #using list compriention ciunting the even number
+#print(f"addition of even number is{nee_array}")        #printing addition of even no. in array
+
+new_array=sum(array)                                  #making sun of array
+print(f"sum og array is:-{new_array}")                        #printing array
